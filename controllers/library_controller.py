e3cf5a1d74466b33478df42636127eb0ba918927
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2023-02-01 22:15:32 +0000

    everything working bar edit

--- controllers/library_controller.py
+++ controllers/library_controller.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from repositories import author_repository
 from repositories import book_repository
 from models.book import Book
+from models.author import Author
 
 library_blueprint = Blueprint('books', __name__)
 
@@ -17,6 +18,39 @@ def books():
 def authors():
     authors = author_repository.select_all()
     return render_template('books/authors.html', authors=authors)
+
+@library_blueprint.route('/books/add-author', methods=['GET'])
+def new_author():
+    return render_template('/books/add-author.html')
+
+
+
+# New author
+@library_blueprint.route('/authors', methods=['POST'])
+def create_author():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    author_repository.save(Author(first_name, last_name))
+    return redirect('/authors')
+
+
+# show author
+@library_blueprint.route('/authors/<id>', methods=['GET'])
+def show_author(id):
+    author = author_repository.select(id)
+    # books = book_repository.select_all()
+    return render_template('books/show-author.html', author=author)
+
+
+
+
+# delete author
+@library_blueprint.route('/books/<id>/delete', methods=['POST'])
+def delete_author(id):
+    author_repository.delete(id)
+    return redirect('/books')
+
+
 
 # New task
 # get request
@@ -44,8 +78,7 @@ def show_book(id):
     authors = author_repository.select_all()
     return render_template('books/show.html', book=book, authors=authors)
 
-# edit task
-# get request
+# edit book
 @library_blueprint.route('/books/<id>/edit', methods=['GET'])
 def edit_book(id):
     book = book_repository.select(id)
@@ -53,8 +86,7 @@ def edit_book(id):
     return render_template('books/edit.html', book=book, authors=authors)
 
 
-# update task
-# post request 
+# update book
 @library_blueprint.route('/books/<id>', methods=['POST'])
 def update_book(id):
     title = request.form['title']
@@ -65,8 +97,7 @@ def update_book(id):
     return redirect('/books')
 
 
-# delete task
-# post request 
+# delete book
 @library_blueprint.route('/books/<id>/delete', methods=['POST'])
 def delete_book(id):
     book_repository.delete(id)
